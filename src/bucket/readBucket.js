b313bdd593b8b69aef42f3966ccf3bd3df64ec8e
Andrés Weitzel <andres96energy@hotmail.com>
2023-03-12 14:27:10 -0300

    Update helpers

---
+++ src/bucket/readBucket.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//External
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { sdkStreamMixin } = require("@aws-sdk/util-stream-node");
+//Const/Vars
+let objectString;
+
+const get = async()=> {
+  //Checks
+  objectString = "";
+
+  try {
+    const client = new S3Client({
+      forcePathStyle: true,
+      credentials: {
+        // This specific key is required when working offline
+        accessKeyId: "S3RVER",
+        secretAccessKey: "S3RVER",
+      },
+      endpoint: "http://localhost:4569",
+    });
+
+    let resp = await client.send(
+      new GetObjectCommand({
+        Bucket: "local-bucket",
+        Key: "bucketS3.json",
+      })
+    );
+
+    // this throws if Body is undefined
+    objectString = await sdkStreamMixin(resp.Body).transformToString();
+
+    return objectString;
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { get };
